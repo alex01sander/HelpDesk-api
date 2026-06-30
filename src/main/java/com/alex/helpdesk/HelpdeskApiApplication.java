a07315ec0cdf0402a1017a69a5b03dd735a4497b
Alex Britto <alexsander01@hotmail.com.br>
2026-06-30 17:33:15 -0300

    feat: configure initial structure for HelpDesk API

---
+++ src/main/java/com/alex/helpdesk/HelpdeskApiApplication.java
@@ -0,0 +1,13 @@
+package com.alex.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/alex/helpdesk/dto/UsuarioResponseDTO.java
@@ -0,0 +1,8 @@
+package com.alex.helpdesk.dto;
+
+public record UsuarioResponseDTO(
+        Long id,
+        String nome,
+        String email
+) {
+}
